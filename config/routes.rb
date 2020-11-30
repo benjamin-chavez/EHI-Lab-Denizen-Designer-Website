6003aa2804b059583641c55e63a41736dc6dbd45
Ben Chavez <ben.m.chavez@gmail.com>
2020-11-30 14:35:22 -0600

    fixed routing'

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/denizendesigner/home", to: 'pages#home' 
+  get "/denizendesigner/interviews", to: 'pages#home' 
+  get "/denizendesigner/interviews/:id", to: 'pages#home' 
+  get "/denizendesigner/report", to: 'pages#home' 
+  get "/denizendesigner/designerdatabase", to: 'pages#home' 
+  get "/denizendesigner/quotes", to: 'pages#home' 
+  get "/denizendesigner/resources", to: 'pages#home' 
 end
