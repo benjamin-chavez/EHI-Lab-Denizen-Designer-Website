33a1ff3b63a2e33edafdf2a742309a7b299121d4
Ben Chavez <ben.m.chavez@gmail.com>
2020-11-30 13:54:48 -0600

    react injection

---
+++ app/javascript/denizenDesigner/components/app_denizen.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AppDenizen = (props) => {
+  return (
+    <div>
+      <p>test home page</p>
+    </div>
+  );
+};
+
+export default AppDenizen;
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,9 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -16,7 +14,6 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-
 // ----------------------------------------------------
 // Note(lewagon): ABOVE IS RAILS DEFAULT CONFIGURATION
 // WRITE YOUR OWN JS STARTING FROM HERE 👇
@@ -24,11 +21,12 @@ require("channels")
 
 // External imports
 import "bootstrap";
+import "../denizenDesigner";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
-document.addEventListener('turbolinks:load', () => {
+document.addEventListener("turbolinks:load", () => {
   // Call your functions here, e.g:
   // initSelect2();
 });
