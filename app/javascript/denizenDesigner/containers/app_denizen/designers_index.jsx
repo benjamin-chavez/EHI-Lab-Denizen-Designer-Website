0ddf3db87294f4f8f3a6b73641d3ce56c2031969
Ben Chavez <ben.m.chavez@gmail.com>
2021-02-15 19:10:40 -0600

    Designer database - new cards

--- app/javascript/denizenDesigner/containers/app_denizen/designers_index.jsx
+++ app/javascript/denizenDesigner/containers/app_denizen/designers_index.jsx
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { Card } from 'react-bootstrap';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import { Card, Row, Col, Image } from 'react-bootstrap';
+import igIcon from '../../../../assets/images/igIcon.svg';
+import twitterIcon from '../../../../assets/images/twitterIcon.svg';
+import mailIcon from '../../../../assets/images/mailIcon.svg';
 
 import { fetchParticipants } from '../../actions';
 
@@ -20,7 +21,7 @@ class DesignersIndex extends Component {
           {participant.location == this.props.city ||
           this.props.city == 'designerdatabase' ? (
             <div>
-              <Row className='mb-2'>
+              {/* <Row className='mb-2'>
                 <Col className='px-0'>
                   <Card className='database-card'>
                     <div className='py-2 px-3'>
@@ -38,6 +39,55 @@ class DesignersIndex extends Component {
                     </div>
                   </Card>
                 </Col>
+              </Row> */}
+              <Row className='designer-database'>
+                {/* {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14].map((i) => ( */}
+                {this.props.participants.map((participant) => (
+                  <Col xs={12} sm={12} md={4} lg={3} xl={3} className=''>
+                    <Row>
+                      <span className='participant-name pl-1 pr-3'>
+                        {participant.first_name}
+                        {participant.last_name}
+                      </span>
+                    </Row>
+                    <Row>
+                      <span className='role pl-1'>
+                        {participant.designer_type}
+                      </span>
+                    </Row>
+                    <Row>
+                      <ul className='data-list pl-1'>
+                        <li>
+                          <Image
+                            src={twitterIcon}
+                            className='d-inline-block align-top'
+                            alt='Twitter Icon'
+                            fluid
+                          />{' '}
+                          Mark_1234
+                        </li>
+                        <li>
+                          <Image
+                            src={mailIcon}
+                            className='d-inline-block align-top'
+                            alt='Email Icon'
+                            fluid
+                          />{' '}
+                          Mark1234@usbc.edu
+                        </li>
+                        <li>
+                          <Image
+                            src={igIcon}
+                            className='d-inline-block align-top'
+                            alt='Instagram Icon'
+                            fluid
+                          />{' '}
+                          <span> Mark_1234</span>
+                        </li>
+                      </ul>
+                    </Row>
+                  </Col>
+                ))}
               </Row>
             </div>
           ) : (
@@ -51,7 +101,7 @@ class DesignersIndex extends Component {
   render() {
     return (
       <div>
-        <Row className='mb-3'>
+        {/* <Row className='mb-3'>
           <Col xs={3} className=''>
             <span className='designer-database-col-headers'>Name</span>
           </Col>
@@ -61,7 +111,7 @@ class DesignersIndex extends Component {
           <Col xs={5} className=''>
             <span className='designer-database-col-headers'>Contact</span>
           </Col>
-        </Row>
+        </Row> */}
         <div className=''>{this.renderParticipants()}</div>
       </div>
     );
