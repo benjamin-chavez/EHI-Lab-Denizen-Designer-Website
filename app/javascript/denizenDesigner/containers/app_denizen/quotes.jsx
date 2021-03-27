e0d2d03867fcdcab6b40c288d3f049f45f0476ac
Ben Chavez <ben.m.chavez@gmail.com>
2021-03-27 18:17:11 -0500

    added quote categories

--- app/javascript/denizenDesigner/containers/app_denizen/quotes.jsx
+++ app/javascript/denizenDesigner/containers/app_denizen/quotes.jsx
@@ -3,15 +3,40 @@ import QuotesIndex from './quotes_index';
 
 class Quotes extends Component {
   render() {
+    let quoteCategories = [
+      'Community Building',
+      'Trust',
+      'Power',
+      'Space Making',
+      'Increasing access',
+      'Knowledge Building',
+      'Responsibility',
+      'Mutual Learning',
+      'Change Making',
+      'Rewards/Upsides/Positives (Need new name)',
+      'Community Empowerment',
+      'Future Implications',
+      'Adaptation',
+      'Design',
+    ];
+
     return (
       <div>
         <div>
-          <ul className='quote-filter-menu'>
+          {/* <ul className='quote-filter-menu'>
             <li className='quote-filter-item selected'>
               Made data meaningful for individuals
             </li>
             <li className='quote-filter-item'>Opportunity to advance</li>
             <li className='quote-filter-item'>Co-design</li>
+          </ul> */}
+          <ul className='quote-filter-menu'>
+            <li className='quote-filter-item selected'>
+              Made data meaningful for individuals
+            </li>
+            {quoteCategories.map((quoteCategory) => {
+              return <li className='quote-filter-item'>{quoteCategory}</li>;
+            })}
           </ul>
           <QuotesIndex />
         </div>
