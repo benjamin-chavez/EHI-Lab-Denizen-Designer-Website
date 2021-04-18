37d6b8734d5b02aa510e2ee8d4ec6c3a02bdd4f0
Ben Chavez <ben.m.chavez@gmail.com>
2021-04-18 16:36:35 -0500

    spacing tweak

--- app/javascript/denizenDesigner/containers/app_denizen/participants_index.jsx
+++ app/javascript/denizenDesigner/containers/app_denizen/participants_index.jsx
@@ -12,38 +12,6 @@ class ParticipantsIndex extends Component {
     this.props.fetchParticipants();
   }
 
-  // renderParticipants() {
-  //   return this.props.participants.map((participant) => {
-  //     return (
-  //       <Col
-  //         xs='4'
-  //         // sm={6}
-  //         // md={6}
-  //         // lg={4}
-  //         className='mb-4 '
-  //         style={{
-  //           maxWidth: '15.241rem',
-  //           // maxHeight: '8.938rem',
-  //           // justifyContent: 'space-between',
-  //         }}
-  //       >
-  //         <Card style={{ maxWidth: '15.241rem', maxHeight: '8.938rem' }}>
-  //           <Link>
-  //             <Card.Img src='https://res.cloudinary.com/dyy8g76av/image/upload/v1611545967/Denizen%20Designer%20%28Temp%29/participant_tjkgwb.jpg' />
-  //             {/* <Card.Img src={participant.profileImgLink} /> */}
-  //             <Card.Body>
-  //               <Card.ImgOverlay>
-  //                 <Card.Title> Card Title</Card.Title>
-  //                 <Card.Text>Testing123</Card.Text>
-  //               </Card.ImgOverlay>
-  //             </Card.Body>
-  //           </Link>
-  //         </Card>
-  //       </Col>
-  //     );
-  //   });
-  // }
-
   render() {
     return (
       <div>
@@ -88,69 +56,6 @@ class ParticipantsIndex extends Component {
           </Row>
         </Container>
       </div>
-
-      // <div className=''>
-      //   <Row>
-      //     {/* <Col> */}
-      //     {/* <div className='participant-list'>{this.renderParticipants()}</div> */}
-      //     <div className=''>{this.renderParticipants()}</div>
-      //     {/* </Col> */}
-      //   </Row>
-
-      //   <div class='row'>
-      //     <Col xs={6} sm={6} md={6} lg={4} className='mb-4'>
-      //       <Card style={{ maxWidth: '15.241rem', maxHeight: '8.938rem' }}>
-      //         <Link>
-      //           <Card.Img src='https://res.cloudinary.com/dyy8g76av/image/upload/v1593735159/borderlinebasic2_z0e0fw.png' />
-      //           <Card.Body>
-      //             <Card.ImgOverlay>
-      //               <Card.Title> Card Title</Card.Title>
-      //               <Card.Text>Testing123</Card.Text>
-      //             </Card.ImgOverlay>
-      //           </Card.Body>
-      //         </Link>
-      //       </Card>
-      //     </Col>
-      //   </div>
-
-      //   <div class='row'>
-      //     <div class='col-lg-4 col-md-6 col-sm-12 mb-4'>
-      //       <div class='card'>
-      //         <a href='https://borderlinebasicb.com/' target='_blank'>
-      //           <img
-      //             src='https://res.cloudinary.com/dyy8g76av/image/upload/v1593735159/borderlinebasic2_z0e0fw.png'
-      //             class='card-img img-fluid'
-      //             alt='Borderline Basic Website Preview'
-      //           />
-
-      //           <div class='card-img-overlay card-body'>
-      //             <div>
-      //               <h2 class='card-title'>Borderline Basic</h2>
-      //               <div class='container'>
-      //                 <p class='card-text'>
-      //                   A custom blogging application for writer Brianna Webb.
-      //                 </p>
-      //               </div>
-      //             </div>
-      //           </div>
-      //         </a>
-      //       </div>
-      //     </div>
-      //     <Col xs={6} sm={6} md={6} lg={4} className='mb-4'>
-      //       <Card>
-      //         <Link>
-      //           <Card.Img src='https://res.cloudinary.com/dyy8g76av/image/upload/v1593735159/borderlinebasic2_z0e0fw.png' />
-      //           <Card.Body>
-      //             <Card.ImgOverlay>
-      //               <Card.Title> Card Title</Card.Title>
-      //               <Card.Text>Testing123</Card.Text>
-      //             </Card.ImgOverlay>
-      //           </Card.Body>
-      //         </Link>
-      //       </Card>
-      //     </Col>
-      //   </div>
-      // </div>
     );
   }
 }
