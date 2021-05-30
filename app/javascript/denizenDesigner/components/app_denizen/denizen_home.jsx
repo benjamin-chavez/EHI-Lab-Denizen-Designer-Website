a0f2b9f2e915f26bc49743e42c0b4068ddcef3f3
Ben Chavez <ben.m.chavez@gmail.com>
2021-05-30 12:59:19 -0500

    home screen background image styling fixed cropping issue

--- app/javascript/denizenDesigner/components/app_denizen/denizen_home.jsx
+++ app/javascript/denizenDesigner/components/app_denizen/denizen_home.jsx
@@ -7,44 +7,47 @@ import { Row, Col, Jumbotron, Button, Card, Container } from 'react-bootstrap';
 class DenizenHome extends Component {
   render() {
     return (
-      <div className='homeScreen pt-2 pb-3'>
-        <Row className='center mb-3 '>
-          <Col>
-            <h1 className='title'>Design + Community</h1>
-          </Col>
-        </Row>
+      <div className='homeScreenWrapper'>
+        <div className='homeScreen pb-3'>
+          <Row className='center mb-3 '>
+            <Col>
+              <h1 className='title'>Design + Community</h1>
+            </Col>
+          </Row>
 
-        <Row className='center mb-3'>
-          <Col>
-            <p className='textBody'>
-              The Denizen Designer Project documents the insights and
-              experiences of individuals and collectives who have engaged design
-              as a way to address social, economic, or political challenges in
-              their local environments. We interviewed people from this project
-              who come from a variety of backgrounds ranging from graphic design
-              to architecture and consulting. Although their backgrounds and
-              interpretations of design may be different, there is a common
-              theme that participatory design and community work are fundamental
-              aspects to ethical and transformative design. We hope these
-              interviews bring more awareness to this type of work and a
-              stronger network of Denizen Designers will be formed.{' '}
-            </p>
-          </Col>
-        </Row>
-        <Row className='center justify-content-sm-center pb-5'>
-          <Col sm={12} md={5} lg={3}>
-            <Link className='denizen-link' to='/denizendesigner/interviews'>
-              <Button
-                className='homeBtn p-3'
-                variant='light'
-                onMouseDown={(e) => e.preventDefault()}
-                block
-              >
-                Meet The Denizen Designers
-              </Button>
-            </Link>
-          </Col>
-        </Row>
+          <Row className='center mb-3'>
+            <Col>
+              <p className='textBody'>
+                The Denizen Designer Project documents the insights and
+                experiences of individuals and collectives who have engaged
+                design as a way to address social, economic, or political
+                challenges in their local environments. We interviewed people
+                from this project who come from a variety of backgrounds ranging
+                from graphic design to architecture and consulting. Although
+                their backgrounds and interpretations of design may be
+                different, there is a common theme that participatory design and
+                community work are fundamental aspects to ethical and
+                transformative design. We hope these interviews bring more
+                awareness to this type of work and a stronger network of Denizen
+                Designers will be formed.{' '}
+              </p>
+            </Col>
+          </Row>
+          <Row className='center justify-content-sm-center pb-5'>
+            <Col sm={12} md={5} lg={3}>
+              <Link className='denizen-link' to='/denizendesigner/interviews'>
+                <Button
+                  className='homeBtn p-3'
+                  variant='light'
+                  onMouseDown={(e) => e.preventDefault()}
+                  block
+                >
+                  Meet The Denizen Designers
+                </Button>
+              </Link>
+            </Col>
+          </Row>
+        </div>
       </div>
     );
   }
