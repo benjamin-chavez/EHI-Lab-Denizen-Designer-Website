9cb453f6b745667e2e3076704f44602c4e7c0c91
Ben Chavez <ben.m.chavez@gmail.com>
2021-06-11 17:40:31 -0500

    fade in animation on the report

--- app/javascript/denizenDesigner/containers/app_denizen/reportScreen/reportDesktop.jsx
+++ app/javascript/denizenDesigner/containers/app_denizen/reportScreen/reportDesktop.jsx
@@ -31,6 +31,7 @@ import References from './references';
 // import Chart from './reportScreen/raceChart';
 
 import Scroll from 'react-scroll';
+import Fade from 'react-reveal/Fade';
 
 var Link = Scroll.Link;
 var DirectLink = Scroll.DirectLink;
@@ -71,145 +72,149 @@ class ReportDesktop extends Component {
         <Col sm={3} className='report-col-2'>
           {/* <Col sm={3} className='mr-2'> */}
           <ul className='test123'>
-            <li className='reportDownloadButton'>
-              <Button className='pl-0 reportDownloadBtn'>
-                <Link className='' target='_blank' download>
-                  Download
+            <Fade bottom duration={1500} delay={0} count={1}>
+              <li className='reportDownloadButton'>
+                <Button className='pl-0 reportDownloadBtn'>
+                  <Link className='' target='_blank' download>
+                    Download
+                  </Link>
+                </Button>
+              </li>
+              <li className='report-li'>
+                <Link
+                  activeClass='active'
+                  to='executiveSummary'
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                  containerId='containerElement'
+                  // style={{ display: 'inline-block', margin: '20px' }}
+                >
+                  Executive Summary
+                  {/* <Image src={ArrowDown} className='pl-2' /> */}
                 </Link>
-              </Button>
-            </li>
-            <li className='report-li'>
-              <Link
-                activeClass='active'
-                to='executiveSummary'
-                spy={true}
-                smooth={true}
-                duration={500}
-                containerId='containerElement'
-                // style={{ display: 'inline-block', margin: '20px' }}
-              >
-                Executive Summary
-                {/* <Image src={ArrowDown} className='pl-2' /> */}
-              </Link>
-            </li>
-            <li className='report-li'>
-              <Link
-                activeClass='active'
-                to='overview'
-                spy={true}
-                smooth={true}
-                duration={500}
-                containerId='containerElement'
-              >
-                Overview
-                {/* <Image src={ArrowDown} className='pl-2' /> */}
-              </Link>
-            </li>
-            <li className='report-li'>
-              <Link
-                activeClass='active'
-                to='introduction'
-                spy={true}
-                smooth={true}
-                duration={500}
-                containerId='containerElement'
-              >
-                Introduction
-                {/* <Image src={ArrowDown} className='pl-2' /> */}
-              </Link>
-            </li>
-            <li className='report-li'>
-              <Link
-                activeClass='active'
-                to='researchProcess'
-                spy={true}
-                smooth={true}
-                duration={500}
-                containerId='containerElement'
-              >
-                Research Process
-                {/* <Image src={ArrowDown} className='pl-2' /> */}
-              </Link>
-            </li>
-            <li className='report-li'>
-              <Link
-                activeClass='active'
-                to='demographics'
-                spy={true}
-                smooth={true}
-                duration={500}
-                containerId='containerElement'
-              >
-                Demographics
-                {/* <Image src={ArrowDown} className='pl-2' /> */}
-              </Link>
-            </li>
-            <li className='report-li'>
-              <Link
-                activeClass='active'
-                to='summary'
-                spy={true}
-                smooth={true}
-                duration={500}
-                containerId='containerElement'
-              >
-                Summary & Key Findings
-                {/* <Image src={ArrowDown} className='pl-2' /> */}
-              </Link>
-            </li>
-            <li className='report-li '>
-              <Link
-                activeClass='active'
-                to='conclusions'
-                spy={true}
-                smooth={true}
-                duration={500}
-                containerId='containerElement'
-              >
-                Conclusions & Key Recommendations
-                {/* <Image src={ArrowDown} className='pl-2 ' /> */}
-              </Link>
-            </li>
-            <li className='report-li '>
-              <Link
-                activeClass='active'
-                to='references'
-                spy={true}
-                smooth={true}
-                duration={500}
-                containerId='containerElement'
-              >
-                References
-                {/* <Image src={ArrowDown} className='pl-2 ' /> */}
-              </Link>
-            </li>
-            {/* ****************************************************** */}
+              </li>
+              <li className='report-li'>
+                <Link
+                  activeClass='active'
+                  to='overview'
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                  containerId='containerElement'
+                >
+                  Overview
+                  {/* <Image src={ArrowDown} className='pl-2' /> */}
+                </Link>
+              </li>
+              <li className='report-li'>
+                <Link
+                  activeClass='active'
+                  to='introduction'
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                  containerId='containerElement'
+                >
+                  Introduction
+                  {/* <Image src={ArrowDown} className='pl-2' /> */}
+                </Link>
+              </li>
+              <li className='report-li'>
+                <Link
+                  activeClass='active'
+                  to='researchProcess'
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                  containerId='containerElement'
+                >
+                  Research Process
+                  {/* <Image src={ArrowDown} className='pl-2' /> */}
+                </Link>
+              </li>
+              <li className='report-li'>
+                <Link
+                  activeClass='active'
+                  to='demographics'
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                  containerId='containerElement'
+                >
+                  Demographics
+                  {/* <Image src={ArrowDown} className='pl-2' /> */}
+                </Link>
+              </li>
+              <li className='report-li'>
+                <Link
+                  activeClass='active'
+                  to='summary'
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                  containerId='containerElement'
+                >
+                  Summary & Key Findings
+                  {/* <Image src={ArrowDown} className='pl-2' /> */}
+                </Link>
+              </li>
+              <li className='report-li '>
+                <Link
+                  activeClass='active'
+                  to='conclusions'
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                  containerId='containerElement'
+                >
+                  Conclusions & Key Recommendations
+                  {/* <Image src={ArrowDown} className='pl-2 ' /> */}
+                </Link>
+              </li>
+              <li className='report-li '>
+                <Link
+                  activeClass='active'
+                  to='references'
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                  containerId='containerElement'
+                >
+                  References
+                  {/* <Image src={ArrowDown} className='pl-2 ' /> */}
+                </Link>
+              </li>
+              {/* ****************************************************** */}
+            </Fade>
           </ul>
         </Col>
         {/* <div className='mr-4'></div> */}
         <Col sm={9} className='report-col-9'>
           {/* <Container className='report-container'> */}
-          <Element
-            name='test7'
-            className='report-container'
-            id='containerElement'
-            style={{
-              // position: 'relative',
-              // height: '100%',
-              width: '100%',
-              overflow: 'scroll',
-              // marginBottom: '100px',
-            }}
-          >
-            <ExecutiveSummary />
-            <Overview />
-            <Introduction />
-            <ResearchProcess />
-            <Demographics />
-            <Summary />
-            <Conclusions />
-            <References />
-          </Element>
+          <Fade duration={1500} delay={500} count={1}>
+            <Element
+              name='test7'
+              className='report-container'
+              id='containerElement'
+              style={{
+                // position: 'relative',
+                // height: '100%',
+                width: '100%',
+                overflow: 'scroll',
+                // marginBottom: '100px',
+              }}
+            >
+              <ExecutiveSummary />
+              <Overview />
+              <Introduction />
+              <ResearchProcess />
+              <Demographics />
+              <Summary />
+              <Conclusions />
+              <References />
+            </Element>
+          </Fade>
         </Col>
       </Row>
     );
